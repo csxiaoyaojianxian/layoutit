1134ac29afc536f19970502321f759b4198d1e38
csxiaoyaojianxian <1724338257@qq.com>
2019-05-28 22:23:18 +0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+/**
+ * 判断是否支持localStorage
+ */
 function supportstorage() {
 	if (typeof window.localStorage=='object') 
 		return true;
@@ -5,28 +8,36 @@ function supportstorage() {
 		return false;		
 }
 
+/**
+ * 自动保存
+ * stopsave 锁字段用于阻止自动保存，成对出现，1上锁
+ */
 function handleSaveLayout() {
 	var e = $(".demo").html();
+	// 未加锁且内容有变化
 	if (!stopsave && e != window.demoHtml) {
 		stopsave++;
 		window.demoHtml = e;
-		saveLayout();
+		saveLayout(); // 具体逻辑
 		stopsave--;
 	}
 }
-
-var layouthistory; 
+/**
+ * 自动保存具体逻辑
+ */
+var layouthistory; // 当前页面最新数据
 function saveLayout(){
 	var data = layouthistory;
 	if (!data) {
-		data={};
-		data.count = 0;
-		data.list = [];
+		data={}; // 组织数据格式
+		data.count = 0; // 记录步数，当前步数
+		data.list = []; // 记录每步的数据
 	}
 	if (data.list.length>data.count) {
 		for (i=data.count;i<data.list.length;i++)
-			data.list[i]=null;
+			data.list[i]=null; // 刷新后删除记录的步数后面多余的记录
 	}
+	// 存当前数据到一条新纪录
 	data.list[data.count] = window.demoHtml;
 	data.count++;
 	if (supportstorage()) {
@@ -45,7 +56,6 @@ function saveLayout(){
 }
 
 function downloadLayout(){
-	
 	$.ajax({  
 		type: "POST",  
 		url: "/build/downloadLayout",  
@@ -63,6 +73,9 @@ function downloadHtmlLayout(){
 	});
 }
 
+/**
+ * 撤销
+ */
 function undoLayout() {
 	var data = layouthistory;
 	//console.log(data);
@@ -87,6 +100,9 @@ function undoLayout() {
 	});*/
 }
 
+/**
+ * 重做
+ */
 function redoLayout() {
 	var data = layouthistory;
 	if (data) {
@@ -112,12 +128,16 @@ function redoLayout() {
 	});*/
 }
 
+/**
+ * 处理各种ids
+ */
 function handleJsIds() {
 	handleModalIds();
 	handleAccordionIds();
 	handleCarouselIds();
 	handleTabsIds()
 }
+// 拖入手风琴处理id
 function handleAccordionIds() {
 	var e = $(".demo #myAccordion");
 	var t = randomNumber();
@@ -135,8 +155,10 @@ function handleAccordionIds() {
 		})
 	})
 }
+// 拖入轮播图处理id
 function handleCarouselIds() {
 	var e = $(".demo #myCarousel");
+	console.log(JSON.stringify(e))
 	var t = randomNumber();
 	var n = "carousel-" + t;
 	e.attr("id", n);
@@ -146,6 +168,7 @@ function handleCarouselIds() {
 	e.find(".left").attr("href", "#" + n);
 	e.find(".right").attr("href", "#" + n)
 }
+// 拖入模态框处理id
 function handleModalIds() {
 	var e = $(".demo #myModalLink");
 	var t = randomNumber();
@@ -155,6 +178,7 @@ function handleModalIds() {
 	e.attr("href", "#" + n);
 	e.next().attr("id", n)
 }
+// 拖入切换卡处理id
 function handleTabsIds() {
 	var e = $(".demo #myTabs");
 	var t = randomNumber();
@@ -167,21 +191,30 @@ function handleTabsIds() {
 		$(t).parent().parent().find("a[href=#" + n + "]").attr("href", "#" + r)
 	})
 }
+// 生成随机数，用于生产id
 function randomNumber() {
+	// 返回指定区间随机数
+	function randomFromInterval(e, t) {
+		return Math.floor(Math.random() * (t - e + 1) + e)
+	}
 	return randomFromInterval(1, 1e6)
 }
-function randomFromInterval(e, t) {
-	return Math.floor(Math.random() * (t - e + 1) + e)
-}
+
+/**
+ * 栅格系统
+ * 拼装模板，校验合法性
+ */
 function gridSystemGenerator() {
 	$(".lyrow .preview input").bind("keyup", function() {
 		var e = 0;
 		var t = "";
-		var n = $(this).val().split(" ", 12);
+		var n = $(this).val().split(" ", 12); // 12数组最大长度
+		// 拼装模板
 		$.each(n, function(n, r) {
-			e = e + parseInt(r);
+			e += parseInt(r);
 			t += '<div class="span' + r + ' column"></div>'
 		});
+		// 列数12，合法才显示
 		if (e == 12) {
 			$(this).parent().next().children().html(t);
 			$(this).parent().prev().show()
@@ -190,13 +223,19 @@ function gridSystemGenerator() {
 		}
 	})
 }
+/**
+ * 元素编辑区 configuration
+ * 【编辑】【无样式】【嵌入】
+ */
 function configurationElm(e, t) {
+	// a标签类型
 	$(".demo").delegate(".configuration > a", "click", function(e) {
 		e.preventDefault();
 		var t = $(this).parent().next().next().children();
 		$(this).toggleClass("active");
 		t.toggleClass($(this).attr("rel"))
 	});
+	// 下拉菜单类型
 	$(".demo").delegate(".configuration .dropdown-menu a", "click", function(e) {
 		e.preventDefault();
 		var t = $(this).parent().parent();
@@ -212,37 +251,58 @@ function configurationElm(e, t) {
 		n.addClass($(this).attr("rel"))
 	})
 }
+/**
+ * 删除一个元素
+ */
 function removeElm() {
 	$(".demo").delegate(".remove", "click", function(e) {
 		e.preventDefault();
 		$(this).parent().remove();
+		// 删除最后一个时候，全清除
 		if (!$(".demo .lyrow").length > 0) {
 			clearDemo()
 		}
 	})
 }
+/**
+ * 完全清除页面和缓存数据
+ */
 function clearDemo() {
 	$(".demo").empty();
 	layouthistory = null;
 	if (supportstorage())
 		localStorage.removeItem("layoutdata");
 }
+// 移除顶部菜单按钮 active
 function removeMenuClasses() {
 	$("#menu-layoutit li button").removeClass("active")
 }
+// 切换自适应宽度和固定宽度
 function changeStructure(e, t) {
 	$("#download-layout ." + e).removeClass(e).addClass(t)
 }
+/**
+ * 去除当前层级嵌套，子元素放入父元素，删除当前层级
+ * @param {*} e 
+ */
 function cleanHtml(e) {
 	$(e).parent().append($(e).children().html())
 }
+/**
+ * 设置下载会话框中的源码
+ * ？？？
+ */
 function downloadLayoutSrc() {
 	var e = "";
+	// .demo 放入 #download-layout
 	$("#download-layout").children().html($(".demo").html());
 	var t = $("#download-layout").children();
+	// 去除不相关的功能标签
 	t.find(".preview, .configuration, .drag, .remove").remove();
+	// 给 .lyrow .box-element 添加标记 .removeClean
 	t.find(".lyrow").addClass("removeClean");
 	t.find(".box-element").addClass("removeClean");
+	// 删除6层、5层，4，3，2，1...
 	t.find(".lyrow .lyrow .lyrow .lyrow .lyrow .removeClean").each(function() {
 		cleanHtml(this)
 	});
@@ -262,11 +322,15 @@ function downloadLayoutSrc() {
 		cleanHtml(this)
 	});
 	t.find(".removeClean").remove();
+	// 去除层级上党 jquery-ui 拖拽
 	$("#download-layout .column").removeClass("ui-sortable");
+	// 去除 column
 	$("#download-layout .row-fluid").removeClass("clearfix").children().removeClass("column");
+	//
 	if ($("#download-layout .container").length > 0) {
 		changeStructure("row-fluid", "row")
 	}
+	// 使用插件清理html
 	formatSrc = $.htmlClean($("#download-layout").html(), {
 		format: true,
 		allowedAttributes: [
@@ -288,27 +352,40 @@ function downloadLayoutSrc() {
 	$("#downloadModal textarea").val(formatSrc)
 }
 
-var currentDocument = null;
-var timerSave = 1000;
-var stopsave = 0;
-var startdrag = 0;
-var demoHtml = $(".demo").html();
-var currenteditor = null;
+// var currentDocument = null;
+var timerSave = 1000; // 自动保存时间间隔
+var stopsave = 0; // 锁，用于阻止自动保存
+var startdrag = 0; // 判断是否正在拖动
+var demoHtml = $(".demo").html(); // 实际html
+var currenteditor = null; // CKEDITOR 中的编辑内容
+
 $(window).resize(function() {
-	$("body").css("min-height", $(window).height() - 90);
-	$(".demo").css("min-height", $(window).height() - 160)
+	setWindowSize();
 });
 
+/**
+ * 从 localstorage 获取数据
+ * 布局数据存储在 layoutdata 字段，JSON 格式
+ * {
+    "count": 3, // 步数，用于撤回
+	"list": []
+ * }
+ */
 function restoreData(){
 	if (supportstorage()) {
 		layouthistory = JSON.parse(localStorage.getItem("layoutdata"));
 		if (!layouthistory) return false;
+		// 获取最新的数据并加载
 		window.demoHtml = layouthistory.list[layouthistory.count-1];
 		if (window.demoHtml) $(".demo").html(window.demoHtml);
 	}
 }
 
+/**
+ * 初始化容器
+ */
 function initContainer(){
+	// 设置右侧面板元素可以拖动排序
 	$(".demo, .demo .column").sortable({
 		connectWith: ".column",
 		opacity: .35,
@@ -324,20 +401,37 @@ function initContainer(){
 	});
 	configurationElm();
 }
+
+/**
+ * 重置窗口大小，设置 min-height
+ */
+function setWindowSize () {
+	$("body").css("min-height", $(window).height() - 90);
+	$(".demo").css("min-height", $(window).height() - 160);
+}
+
+/**
+ * 核心主程序
+ */
 $(document).ready(function() {
+
+	// 初始化 CKEDITOR，传入html源码进行编辑
 	CKEDITOR.disableAutoInline = true;
-	restoreData();
 	var contenthandle = CKEDITOR.replace( 'contenteditor' ,{
 		language: 'zh-cn',
 		contentsCss: ['css/bootstrap-combined.min.css'],
 		allowedContent: true
 	});
-	$("body").css("min-height", $(window).height() - 90);
-	$(".demo").css("min-height", $(window).height() - 160);
+
+	// 加载localstorage数据
+	restoreData();
+	// 重置窗口大小，设置 min-height
+	setWindowSize();
+	// 左侧布局允许拖入右侧工作区（.demo）
 	$(".sidebar-nav .lyrow").draggable({
 		connectToSortable: ".demo",
 		helper: "clone",
-		handle: ".drag",
+		handle: ".drag", // 拖动手柄
 		start: function(e,t) {
 			if (!startdrag) stopsave++;
 			startdrag = 1;
@@ -362,10 +456,11 @@ $(document).ready(function() {
 			startdrag = 0;
 		}
 	});
+	// 左侧组件样式允许拖入右侧布局中的列（.column）
 	$(".sidebar-nav .box").draggable({
 		connectToSortable: ".column",
 		helper: "clone",
-		handle: ".drag",
+		handle: ".drag", // 拖动手柄
 		start: function(e,t) {
 			if (!startdrag) stopsave++;
 			startdrag = 1;
@@ -374,38 +469,53 @@ $(document).ready(function() {
 			t.helper.width(400)
 		},
 		stop: function() {
+			// 处理各种ids
 			handleJsIds();
 			if(stopsave>0) stopsave--;
 			startdrag = 0;
 		}
 	});
+
+	// 初始化容器
 	initContainer();
+
+	// 绑定模板元素的编辑按钮，打开编辑弹窗
 	$('body.edit .demo').on("click","[data-target=#editorModal]",function(e) {
 		e.preventDefault();
+		// 获取实际dom
 		currenteditor = $(this).parent().parent().find('.view');
 		var eText = currenteditor.html();
+		// html源码传入 CKEDITOR 编辑器
 		contenthandle.setData(eText);
 	});
+	// 绑定编辑弹窗的保存按钮
 	$("#savecontent").click(function(e) {
 		e.preventDefault();
+		// 从 CKEDITOR 获取编辑后的html源码
 		currenteditor.html(contenthandle.getData());
 	});
+	// 绑定顶部下载按钮，打开下载弹窗
 	$("[data-target=#downloadModal]").click(function(e) {
 		e.preventDefault();
+		// 设置下载会话框中的源码
 		downloadLayoutSrc();
 	});
+	// 绑定顶部保存按钮，打开保存弹窗
 	$("[data-target=#shareModal]").click(function(e) {
 		e.preventDefault();
 		handleSaveLayout();
 	});
-	$("#download").click(function() {
-		downloadLayout();
-		return false
-	});
-	$("#downloadhtml").click(function() {
-		downloadHtmlLayout();
-		return false
-	});
+
+	// $("#download").click(function() {
+	// 	downloadLayout();
+	// 	return false
+	// });
+	// $("#downloadhtml").click(function() {
+	// 	downloadHtmlLayout();
+	// 	return false
+	// });
+
+	// 绑定编辑按钮 添加 edit 样式
 	$("#edit").click(function() {
 		$("body").removeClass("devpreview sourcepreview");
 		$("body").addClass("edit");
@@ -413,10 +523,7 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		return false
 	});
-	$("#clear").click(function(e) {
-		e.preventDefault();
-		clearDemo()
-	});
+	// 绑定布局编辑 添加 devpreview 样式
 	$("#devpreview").click(function() {
 		$("body").removeClass("edit sourcepreview");
 		$("body").addClass("devpreview");
@@ -424,6 +531,7 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		return false
 	});
+	// 绑定预览按钮 添加 devpreview sourcepreview 样式
 	$("#sourcepreview").click(function() {
 		$("body").removeClass("edit");
 		$("body").addClass("devpreview sourcepreview");
@@ -431,36 +539,55 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		return false
 	});
+	// 绑定清空按钮
+	$("#clear").click(function(e) {
+		e.preventDefault();
+		clearDemo()
+	});
+	// 绑定下载弹窗中 自适应 按钮
 	$("#fluidPage").click(function(e) {
 		e.preventDefault();
+		// 切换 container container-fluid 样式class
 		changeStructure("container", "container-fluid");
 		$("#fixedPage").removeClass("active");
 		$(this).addClass("active");
+		// 设置下载会话框中的源码
 		downloadLayoutSrc()
 	});
+	// 绑定下载弹窗中 固定宽度 按钮
 	$("#fixedPage").click(function(e) {
 		e.preventDefault();
+		// 切换 container container-fluid 样式class
 		changeStructure("container-fluid", "container");
 		$("#fluidPage").removeClass("active");
 		$(this).addClass("active");
+		// 设置下载会话框中的源码
 		downloadLayoutSrc()
 	});
+	// 左侧菜单手风琴
 	$(".nav-header").click(function() {
 		$(".sidebar-nav .boxes, .sidebar-nav .rows").hide();
 		$(this).next().slideDown()
 	});
+	// 绑定撤销
 	$('#undo').click(function(){
-		stopsave++;
+		stopsave++; // 锁
+		// 执行撤销 + 渲染
 		if (undoLayout()) initContainer();
 		stopsave--;
 	});
+	// 绑定重做
 	$('#redo').click(function(){
-		stopsave++;
+		stopsave++; // 锁
+		// 执行重做 + 渲染
 		if (redoLayout()) initContainer();
 		stopsave--;
 	});
+	// 事件委托绑定元素删除按钮
 	removeElm();
+	// 绑定左侧栅格系统功能
 	gridSystemGenerator();
+	// 定时器自动保存
 	setInterval(function() {
 		handleSaveLayout()
 	}, timerSave)
